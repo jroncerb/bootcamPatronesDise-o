79c30bcf26cf1869e0a994df4579c135d9897bf9
jroncerb <jroncerb@emeal.nttdata.com>
2022-10-25 15:06:51 +0200

    proceso

--- src/main/java/singelton/ConstructorPersona.java
+++
@@ -1,5 +0,0 @@
-package singelton;
-
-public class ConstructorPersona {
-
-}
---
+++ src/main/java/singelton/intConstructorPersona.java
@@ -0,0 +1,10 @@
+package singelton;
+
+public interface  intConstructorPersona {
+	
+	public void setname(String name);
+    public  void setage(int age);
+    public void setdni(String dni);
+    public void setdireccion(String direccion);
+
+}
